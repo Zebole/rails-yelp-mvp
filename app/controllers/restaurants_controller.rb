c83cd072276c89a6f9931bfdf13c917e8129cfa2
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-07-28 15:33:50 +0400

    Deletes a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,6 +14,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save!
+    flash[:notice] = "Restaurant has been created!"
     redirect_to restaurant_path(@restaurant)
   end
 
@@ -24,7 +25,15 @@ class RestaurantsController < ApplicationController
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
+    flash[:notice] = "Restaurant has been updated!"
     redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = "Restaurant has been deleted!"
+    redirect_to restaurants_path
   end
 
   private
