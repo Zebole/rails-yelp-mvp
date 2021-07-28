a30b8a69c1d77fc693d043ff63dd5687b0117e48
Tiago Clarenc <tiagoclarenc888@gmail.com>
2021-07-28 16:46:58 +0400

    Adds review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'restaurants/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
